0bc22f3139b59e847d2941f645e72c6c363c13f9
sato <j1716@matsue-ct.jp>
2020-10-30 14:11:44 +0900

    Add Fizz and Buzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,10 @@
 for num in 1..100 do
   if num % 3 == 0 && num % 5 == 0
 	print "FizzBuzz\n"
+  elsif num % 3 == 0
+	print "Fizz\n"
+  elsif num % 5 == 0
+	print "Buaa\n"
   else
 	print "#{num} \n"
   end
